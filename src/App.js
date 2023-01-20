416d397339f1a5d5db9f748594fc959af29b8f49
anriadamia <anriadamia2000@gmail.coom>
2023-01-20 11:33:18 +0400

    test react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  const Sayhello=()=>{
+    return <h1>Hellloooo</h1>
+  }
+  return (
+    <div className="App">
+      hello
+      <Sayhello/>
+    </div>
+  );
+}
+
+export default App;
